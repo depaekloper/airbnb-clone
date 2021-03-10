761057f0063a63b3e81f574f1d99cbcd4650e9cc
hatecloud <seungwoo0910@gmail.com>
2021-03-10 22:43:11 +0900

    #4.4 on_delete, Amenity, Facility, HouseRule Models

--- rooms/models.py
+++ rooms/models.py
@@ -19,6 +19,29 @@ class AbstractItem(core_models.TimeStampedModel):
 
 class RoomType(AbstractItem):
 
+    """ RoomType Object Definition """
+
+    pass
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Object Definition """
+
+    pass
+
+
+class Facility(AbstractItem):
+
+    """ Facility Object Definition """
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Object Definition """
+
     pass
 
 
@@ -40,7 +63,10 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_modes.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(RoomType, blank=True)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rule = models.ManyToManyField(HouseRule, blank=True)
 
     def __str__(self):
         return self.name
