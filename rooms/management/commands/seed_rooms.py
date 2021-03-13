35c2649b01d6f08bc45784b7c552b058e4bfe299
hatecloud <seungwoo0910@gmail.com>
2021-03-13 19:25:40 +0900

    #9.6 seed_reviews

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -8,7 +8,7 @@ from users import models as user_models
 
 class Command(BaseCommand):
 
-    help = "This command creates many users"
+    help = "This command creates rooms"
 
     def add_arguments(self, parser):
         parser.add_argument(
