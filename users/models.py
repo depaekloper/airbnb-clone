7c29f6e58ffe287e94102b5aef7f5dd59b1d521e
hatecloud <seungwoo0910@gmail.com>
2021-03-14 17:46:13 +0900

    #16.4 Completing Verification

--- users/models.py
+++ users/models.py
@@ -60,5 +60,5 @@ class User(AbstractUser):
                 fail_silently=False,
                 html_message=html_message,
             )
-        print(strip_tags(html_message))
+            self.save()
         return
