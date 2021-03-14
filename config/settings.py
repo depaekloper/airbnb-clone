80811d27a839be259d4236f67ab9869dd2021d45
hatecloud <seungwoo0910@gmail.com>
2021-03-14 09:07:19 +0900

    #12.4 Using DetailView CBV

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "^q*jtr^g9)q4@405%$zy2v53yhe652@z=e7u1$$l8d5mm5-eoy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition
