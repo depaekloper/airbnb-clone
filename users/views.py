6689db98a507d5dd7444b8174e319adf1e244c17
hatecloud <seungwoo0910@gmail.com>
2021-03-15 23:00:50 +0900

    #20.10 Signup

--- users/views.py
+++ users/views.py
@@ -33,11 +33,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SighUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "SeungWoo",
-        "last_name": "Paek",
-        "email": "seungwoo0910@gmail.com",
-    }
 
     def form_valid(self, form):
         form.save()
