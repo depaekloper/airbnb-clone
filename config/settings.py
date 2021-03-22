808994aa7d23e0dcd22629228d4caec8d9fb06b3
hatecloud <seungwoo0910@gmail.com>
2021-03-22 21:51:25 +0900

    #26.2 Requirements

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "^q*jtr^g9)q4@405%$zy2v53yhe652@z=e7u1$$l8d5mm5-eoy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG"))
 
 ALLOWED_HOSTS = []
 
